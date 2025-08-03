7ba17f7e1fae7dcf6b4c83840da6308df5f782a5
Nkosinathi-dev1 <nkosinathi.ngele.dev@gmail.com>
2025-08-03 10:36:58 +0200

    add: registered dependency injection added migrations

--- ClientService.API/Program.cs
+++ ClientService.API/Program.cs
@@ -1,9 +1,23 @@
+using ClientService.Application.Interfaces;
+using ClientService.Domain.Interfaces;
+using ClientService.Persistence.Db;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddDbContext<ClientDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//DI
+builder.Services.AddScoped<IClientRepository, ClientService.Persistence.Repositories.ClientRepository>();
+builder.Services.AddScoped<IClientService, ClientService.Application.Services.ClientService>();
+
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
---
+++ ClientService.Persistence/Db/ClientDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientService.Persistence.Db
+{
+    public class ClientDbContextFactory : IDesignTimeDbContextFactory<ClientDbContext>
+    {
+        public ClientDbContext CreateDbContext(string[] args)
+        {
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "..", "ClientService.API");
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ClientDbContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            return new ClientDbContext(optionsBuilder.Options);
+        }
+    }
+}
